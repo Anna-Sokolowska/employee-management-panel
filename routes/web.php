e03e70702c1b155fb4bd4bc7f4a7815ea63d6dc6
Anna-Sokolowska <67736611+Anna-Sokolowska@users.noreply.github.com>
2023-11-13 14:58:56 +0100

    feat(): delete Employee (#8)

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@ Route::controller(EmployeeController::class)->group(function () {
     Route::get('/employees', 'index')->name('employees.index');
     Route::get('/employees/create', 'create')->name('employees.create');
     Route::post('/employees', 'store')->name('employees.store');
-    Route::get('/employees/{id}', 'show')->name('employees.show');
-    Route::get('/employees/{id}/edit', 'edit')->name('employees.edit');
-    Route::put('/employees/{id}', 'update')->name('employees.update');
-    Route::delete('/employees/{id}', 'destroy')->name('employees.destroy');
+    Route::get('/employees/{employee}', 'show')->name('employees.show');
+    Route::get('/employees/{employee}/edit', 'edit')->name('employees.edit');
+    Route::put('/employees/{employee}', 'update')->name('employees.update');
+    Route::delete('/employees/{employee}', 'destroy')->name('employees.destroy');
 });
