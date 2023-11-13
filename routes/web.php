3988c60a41b6066dde720abe08e4f0c9254da58b
Anna-Sokolowska <anna.sokolowska343@gmail.com>
2023-11-13 12:32:20 +0100

    feat(create Employee): add routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(EmployeeController::class)->group(function () {
+    Route::get('/employees', 'index')->name('employees.index');
+    Route::get('/employees/create', 'create')->name('employees.create');
+    Route::post('/employees', 'store')->name('employees.store');
+    Route::get('/employees/{id}', 'show')->name('employees.show');
+    Route::get('/employees/{id}/edit', 'edit')->name('employees.edit');
+    Route::put('/employees/{id}', 'update')->name('employees.update');
+    Route::delete('/employees/{id}', 'destroy')->name('employees.destroy');
 });
