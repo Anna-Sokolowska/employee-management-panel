a3e9f1ac4619f41b0aa561f579ae76e754a50b63
Anna-Sokolowska <67736611+Anna-Sokolowska@users.noreply.github.com>
2023-11-13 12:52:34 +0100

    feat(): create Employee (#7)

---
+++ app/Models/Employee.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Employee extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'company_id',
+        'email',
+        'food_preference_id',
+    ];
+
+    public function phones(): HasMany
+    {
+        return $this->hasMany(PhoneNumber::class);
+    }
+
+    public function company(): BelongsTo
+    {
+        return $this->BelongsTo(Company::class);
+    }
+
+    public function foodPreference(): BelongsTo
+    {
+        return $this->BelongsTo(FoodPreference::class);
+    }
+}
