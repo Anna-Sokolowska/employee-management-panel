de123f8e5bea8e03f3ad4596e045cad1b7beef56
Anna-Sokolowska <anna.sokolowska343@gmail.com>
2023-11-13 21:50:44 +0100

    feat(): add Database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,21 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Company;
+use App\Models\Employee;
+use App\Models\FoodPreference;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $foodPreferences = FoodPreference::factory(7)->create();
+        $companies = Company::factory(15)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Employee::factory(35)
+            ->for($companies->random())
+            ->for($foodPreferences->random())
+            ->create();
     }
 }
