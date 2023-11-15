11307c46d49222a305ddc24277445295cae8705d
Anna-Sokolowska <anna.sokolowska343@gmail.com>
2023-11-15 10:36:00 +0100

    fix(): add old function to inputs

--- resources/views/employees/edit.blade.php
+++ resources/views/employees/edit.blade.php
@@ -6,21 +6,21 @@
         @method('PUT')
         <div class="col-md-6">
             <label for="inputFirstName" class="form-label">{{ __('First name') }}</label>
-            <input type="text" class="form-control" id="inputFirstName" name="first_name" value="{{ $employee->first_name }}">
+            <input type="text" class="form-control" id="inputFirstName" name="first_name" value="{{ old('first_name', $employee->first_name) }}">
         </div>
         <div class="col-md-6">
             <label for="inputLastName" class="form-label">{{ __('Last name') }}</label>
-            <input type="text" class="form-control" id="inputLastName" name="last_name" value="{{ $employee->last_name }}">
+            <input type="text" class="form-control" id="inputLastName" name="last_name" value="{{ old('last_name', $employee->last_name) }}">
         </div>
         <div class="col-12">
             <label for="inputEmail" class="form-label">{{ __('Email') }}</label>
-            <input type="email" class="form-control" id="inputEmail" name="email" value="{{ $employee->email }}">
+            <input type="email" class="form-control" id="inputEmail" name="email" value="{{ old('email', $employee->email) }}">
         </div>
         <div class="col-12">
             <label for="inputCompany" class="form-label">{{ __('Company') }}</label>
             <select id="inputCompany" class="form-select" name="company_id">
                 @foreach ($companies as $company)
-                    <option value="{{ $company->id }}" @selected($employee->company_id == $company->id)>{{ $company->name }}</option>
+                    <option value="{{ $company->id }}" @selected(old('company_id', $employee->company_id) == $company->id)>{{ $company->name }}</option>
                 @endforeach
             </select>
         </div>
@@ -28,7 +28,7 @@
             <label for="inputFoodPreference" class="form-label">{{ __('Food preferences') }}</label>
             <select id="inputFoodPreference" class="form-select" name="food_preference_id">
                 @foreach ($foodPreferences as $foodPreference)
-                    <option value="{{ $foodPreference->id }}" @selected($employee->food_preference_id == $foodPreference->id)>{{ $foodPreference->name }}</option>
+                    <option value="{{ $foodPreference->id }}" @selected(old('food_preference_id', $employee->food_preference_id) == $foodPreference->id)>{{ $foodPreference->name }}</option>
                 @endforeach
             </select>
         </div>
