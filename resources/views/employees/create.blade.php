d654bed06fe02d44b5ccabcf39d1a1c67a3776b8
Anna-Sokolowska <anna.sokolowska343@gmail.com>
2023-11-16 19:25:05 +0100

    feat(): add frontend validation

--- resources/views/employees/create.blade.php
+++ resources/views/employees/create.blade.php
@@ -1,26 +1,26 @@
 @extends('layouts.app')
-
+@section('title')
+    Create Employee
+@endsection
 @section('content')
-    <form class="row g-3" method="POST" action="{{ route('employees.store') }}">
+    <form class="row g-3 needs-validation" method="POST" action="{{ route('employees.store') }}" novalidate>
         @csrf
         <div class="col-md-6">
             <label for="inputFirstName" class="form-label">{{ __('First name') }}</label>
-            <input type="text" class="form-control" id="inputFirstName" name="first_name" value="{{ old('first_name') }}">
-            @error('title')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
+            <input type="text" class="form-control" id="inputFirstName" name="first_name" value="{{ old('first_name') }}" required>
         </div>
         <div class="col-md-6">
             <label for="inputLastName" class="form-label">{{ __('Last name') }}</label>
-            <input type="text" class="form-control" id="inputLastName" name="last_name" value="{{ old('last_name') }}">
+            <input type="text" class="form-control" id="inputLastName" name="last_name" value="{{ old('last_name') }}" required>
         </div>
         <div class="col-12">
             <label for="inputEmail" class="form-label">{{ __('Email') }}</label>
-            <input type="email" class="form-control" id="inputEmail" name="email" value="{{ old('email') }}">
+            <input type="email" class="form-control" id="inputEmail" name="email" value="{{ old('email') }}" required>
         </div>
         <div class="col-12">
             <label for="inputCompany" class="form-label">{{ __('Company') }}</label>
-            <select id="inputCompany" class="form-select" name="company_id">
+            <select id="inputCompany" class="form-select" name="company_id" required>
+                <option disabled selected value="">{{ __('Select company') }}</option>
                 @foreach ($companies as $company)
                     <option value="{{ $company->id }}" @selected(old('company_id') == $company->id)>{{ $company->name }}</option>
                 @endforeach
@@ -28,7 +28,8 @@
         </div>
         <div class="col-12">
             <label for="inputFoodPreference" class="form-label">{{ __('Food preferences') }}</label>
-            <select id="inputFoodPreference" class="form-select" name="food_preference_id">
+            <select id="inputFoodPreference" class="form-select" name="food_preference_id" required>
+                <option disabled selected value="">{{ __('Select food preference') }}</option>
                 @foreach ($foodPreferences as $foodPreference)
                     <option value="{{ $foodPreference->id }}" @selected(old('food_preference_id') == $foodPreference->id)>{{ $foodPreference->name }}</option>
                 @endforeach
@@ -42,8 +43,9 @@
             <label for="inputPhone2" class="form-label">{{ __('Phones 2') }}</label>
             <input type="text" class="form-control" id="inputPhone2"  name="phones[]" value="{{ old('phones.1') }}">
         </div>
-        <div class="col-12">
-            <button type="submit" class="btn btn-primary">{{ __('Create employees') }}</button>
+        <div class="col-12 d-flex justify-content-end gap-2">
+            <button type="submit" class="btn btn-primary">{{ __('Create') }}</button>
+            <a href="{{ route('employees.index') }}" class="btn btn-secondary">{{ __('Back') }}</a>
         </div>
 
         @if ($errors->any())
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,14 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Employee</title>
+    <title>Employee Management Panel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <header>
-    <nav class="navbar bg-body-tertiary border-bottom border-body mb-5">
+    <nav class="navbar bg-body-tertiary border-bottom border-body mb-3">
         <div class="container">
-            <a class="navbar-brand">{{ __('Navbar') }}</a>
+            <a class="navbar-brand" href="{{ route('employees.index') }}">{{ __('Home') }}</a>
         </div>
     </nav>
 </header>
@@ -20,5 +19,24 @@
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+<script>
+    (function () {
+        'use strict'
+
+        var forms = document.querySelectorAll('.needs-validation')
+
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+            })
+    })()
+</script>
 </html>
 
